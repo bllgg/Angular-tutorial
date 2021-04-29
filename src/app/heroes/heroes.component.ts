08ac58871d7be8946f2d4a241247a0473916d2f1
bllgg <gunathilakebllg@gmail.com>
2021-04-29 16:53:03 +0530

    Revert "Added the message service"
    This reverts commit 47b913d4acf9c0557d344f00e80fe4dfbface5e9.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -14,7 +13,7 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor(private heroService: HeroService, private messageService: MessageService) {}
+  constructor(private heroService: HeroService) {}
 
   ngOnInit() {
     this.getHeroes();
@@ -23,12 +22,10 @@ export class HeroesComponent implements OnInit {
   
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   onClear(): void{
     this.selectedHero = undefined;
-    this.messageService.add('Selection was cleared.')
   }
 
   getHeroes(): void {
